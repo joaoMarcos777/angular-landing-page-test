6fa0407186a24477cb4729177889b478b462d79d
João Marcos <joaomarcos@frwk.com.br>
2024-11-03 18:20:34 -0300

    feat: add last section and footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,8 @@ import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { ButtonPrimaryComponent } from '../button-primary/button-primary.component';
 import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +14,8 @@ import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.comp
     NgOptimizedImage,
     ButtonPrimaryComponent,
     NewsletterFormComponent,
+    BenefitComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
